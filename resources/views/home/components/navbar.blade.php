6e3b89d67bb8750425d4e793205bdf90a6d4ba5e
Amharrayadin <amharrayadin@gmail.com>
2023-03-23 19:27:23 +0800

    ray first

--- resources/views/home/components/navbar.blade.php
+++ resources/views/home/components/navbar.blade.php
@@ -1,3 +1,7 @@
+@php
+    $profil = App\Models\Menu::where('kategory', 'profil')->get();
+    $akademik = App\Models\Menu::where('kategory', 'akademik')->get();
+@endphp
 <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top py-0 pe-5">
     <a href="index.html" class="navbar-brand ps-5 me-0">
         <h1 class="text-white m-0">Industro</h1>
@@ -13,21 +17,27 @@
     </style>
     <div class="collapse navbar-collapse" id="navbarCollapse">
         <div class="navbar-nav ms-auto p-1 p-lg-0">
-            <a href="index.html" class="nav-item nav-link active">Home</a>
-            <a href="about.html" class="nav-item nav-link">About</a>
-            <a href="service.html" class="nav-item nav-link">Services</a>
+            <a href="{{ url('/') }}" class="nav-item nav-link {{ request()->is('/') ? 'active' : '' }}">Home</a>
             <div class="nav-item dropdown">
-                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                <a href="#" class="nav-link dropdown-toggle {{ request()->is('profil*') ? 'active' : '' }}"
+                    data-bs-toggle="dropdown">Profil</a>
                 <div class="dropdown-menu bg-light m-0">
-                    <a href="project.html" class="dropdown-item">Projects</a>
-                    <a href="feature.html" class="dropdown-item">Features</a>
-                    <a href="team.html" class="dropdown-item">Our Team</a>
-                    <a href="testimonial.html" class="dropdown-item">Testimonial</a>
-                    <a href="404.html" class="dropdown-item">404 Page</a>
+                    @foreach ($profil as $pf)
+                        <a href="{{ url("/profil/$pf->slug") }}"
+                            class="dropdown-item {{ request()->is("profil/$pf->slug") ? 'active' : '' }}">{{ $pf->title }}</a>
+                    @endforeach
                 </div>
             </div>
-            <a href="contact.html" class="nav-item nav-link">Contact</a>
+            <div class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle {{ request()->is('akademik*') ? 'active' : '' }}"
+                    data-bs-toggle="dropdown">Akademik</a>
+                <div class="dropdown-menu bg-light m-0">
+                    @foreach ($akademik as $akd)
+                        <a href="{{ url("/akademik/$akd->slug") }}"
+                            class="dropdown-item {{ request()->is("akademik/$akd->slug") ? 'active' : '' }}">{{ $akd->title }}</a>
+                    @endforeach
+                </div>
+            </div>
         </div>
-        <a href="" class="btn btn-primary px-3 d-none d-lg-block">Get A Quote</a>
     </div>
 </nav>
